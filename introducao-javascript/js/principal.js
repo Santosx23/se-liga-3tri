55307da2021c237d2bf4fdf4a943a834ca094a26
Santosx23 <130380761+Santosx23@users.noreply.github.com>
2023-12-04 08:54:30 -0300

    Add files via upload

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,11 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+console.log(paciente); // tr
+console.log(tdPeso); // td que tem peso 
+console.log(peso); // Obter 100
